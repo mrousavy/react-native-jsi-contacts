8720f5192eb59cf1b509cb8f19a93dcf65e59454
Marc Rousavy <marcrousavy@hotmail.com>
2021-09-30 12:18:49 +0200

    Add contacts permission

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,17 +1,31 @@
 import * as React from 'react';
-
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Alert } from 'react-native';
 import { Contact, getContactsAsync } from 'react-native-jsi-contacts';
+import { check, PERMISSIONS, request } from 'react-native-permissions';
 
 export default function App() {
   const [result, setResult] = React.useState<Contact[]>([]);
 
-  React.useEffect(() => {
-    getContactsAsync().then((contacts) => {
-      console.log(`Got: ${JSON.stringify(contacts)}`);
-      setResult(contacts);
-    });
+  const load = React.useCallback(async () => {
+    const permission = await check(PERMISSIONS.ANDROID.READ_CONTACTS);
+    if (permission !== 'granted') {
+      const requestResult = await request(PERMISSIONS.ANDROID.READ_CONTACTS);
+      if (requestResult !== 'granted') {
+        Alert.alert(
+          'Permission denied!',
+          'Permission to access contacts has been denied.'
+        );
+      }
+    }
+    console.log(`Contacts Permission: ${permission}`);
+    const contacts = await getContactsAsync();
+    console.log(`Got: ${JSON.stringify(contacts)}`);
+    setResult(contacts);
   }, []);
+
+  React.useEffect(() => {
+    load();
+  }, [load]);
 
   const str = React.useMemo(
     () => result.map((c) => c.firstName).join(', '),
